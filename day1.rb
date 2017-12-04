6941d68ad7a5246d3806ade40779102f7a80a994
Marc Roberts <marc@neutroncreations.com>
2017-12-04 20:34:41 +0000

    finish day 1 part 2, d’oh

--- day1.rb
+++ day1.rb
@@ -2,7 +2,7 @@
 #
 # ./day1.rb <INPUT>
 
-input = ARGV[0].to_i
+input = ARGV[0]
 
 chars = input.scan(/./)
 
@@ -11,8 +11,20 @@ prev = input[-1]
 count = 0
 
 chars.each do |char|
-	count += char.to_i if char == prev
-	prev = char
+  count += char.to_i if char == prev
+  prev = char
 end
 
-print count
+puts "Part I: #{count}"
+
+# PART II
+
+count = 0
+
+length = chars.size
+
+chars.each_with_index do |char, index|
+  count += char.to_i if char == chars.rotate(length / 2)[index]
+end
+
+puts "Part II: #{count}"
