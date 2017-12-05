5051b9c282702cba763cf0947d7cc182d87837d1
Marc Roberts <marc@neutroncreations.com>
2017-12-05 14:11:27 +0000

    day 3 part 2

--- day3.rb
+++ day3.rb
@@ -39,5 +39,65 @@ instructions[0..(input-1)].each { |i| counts[i] += 1 }
 
 distance = (counts['U'] - counts['D']).abs + (counts['R'] - counts['L']).abs
 
-print distance
+puts "Part I: #{distance}"
 
+# PART II
+
+directions = ['R','U','L','D']
+
+# initialise the grid
+size = 20
+
+grid = Array.new(size) do |row|
+  Array.new(size) { 0 }
+end
+
+x = 10
+y = 10
+
+direction_count = 1
+
+grid[x][y] = 1
+
+found = false
+
+loop do
+
+  direction_count.times do
+
+    case directions[0]
+    when 'U'
+      y += 1
+    when 'D'
+      y -= 1
+    when 'R'
+      x += 1
+    when 'L'
+      x -= 1
+    end
+
+    grid[x][y] = grid[x-1][y+1] + grid[x][y+1] + grid[x+1][y+1] +
+                 grid[x-1][y] + grid[x+1][y] +
+                 grid[x-1][y-1] + grid[x][y-1] + grid[x+1][y-1]
+
+    found = true and break if grid[x][y] > input
+  end
+
+  break if found
+
+  if directions[0] == 'U' || directions[0] == 'D'
+    direction_count += 1
+  end
+
+  directions.rotate!
+end
+
+# Dump the grid to a csv
+require 'csv'
+CSV.open("day3.csv", "w") do |csv|
+  grid.each do |row|
+    csv << row
+  end
+end
+
+puts "Part II: #{grid[x][y]}"
